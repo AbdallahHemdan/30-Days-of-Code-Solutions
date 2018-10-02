0ae37e7c3927d6b3f3e317c84ebccfd814395e83
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-10-02 23:00:15 +0200

    Create Day 0: Hello, World.cpp

---
+++ Day 0: Hello, World.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main (){
+string str ; 
+getline (cin,str);
+cout <<"Hello, World." <<endl  ;
+cout << str << endl ; 
+}
